d35263c9464ee3ed98281d596db86ec6886adcca
Iker <iescman2002@gmail.com>
2025-10-06 12:11:04 +0200

    Ejercicio1 Finalizado y modificacion gitignore

--- Practica2/src/Ejercicio1.java
+++ Practica2/src/Ejercicio1.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main (String[]ars) {
         Scanner s = new Scanner (System.in);
+
         int[] arrayInt = new int[10];
         for (int i=0;i<10;i++) {
-            System.out.println("Introduce el siguiente valor: ");
+            System.out.print("Introduce el siguiente valor: ");
             arrayInt[i] = s.nextInt();
+        }
+        for (int i=0;i< arrayInt.length;i++) {
+            System.out.println("Posición "+i+": "+arrayInt[i]);
         }
     }
 }
