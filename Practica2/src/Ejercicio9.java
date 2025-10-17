4561cc49db533eacc9db34f3c40d3becce157b97
Iker <iescman2002@gmail.com>
2025-10-17 09:10:05 +0200

    Ejercicio9 terminado.

--- Practica2/src/Ejercicio9.java
+++ Practica2/src/Ejercicio9.java
@@ -4,15 +4,29 @@ public class Ejercicio9 {
     public static void main(String[] ars) {
         Scanner s = new Scanner(System.in);
         int[] arrayInt = new int[10];
-        String valor = "";
-        int[] posicion = new int[arrayInt.length];
+        int posicion_max = 0;
+        int posicion_min = 0;
 
         for (int i = 0; i < 10; i++) {
             System.out.print("Introduzca el numero: ");
             arrayInt[i] = s.nextInt();
-            valor += arrayInt[i];
         }
-        System.out.print(valor);
+
+        int max = arrayInt[0];
+        int min = max;
+
+        for (int i=0;i < 10; i++) {
+//            System.out.println("posicion: "+i+" valor: "+arrayInt[i]);
+            if (arrayInt[i]>max) {
+                max = arrayInt[i];
+                posicion_max = i;
+            }
+            if (arrayInt[i]<min) {
+                min = arrayInt[i];
+                posicion_min = i;
+            }
+        }
+        System.out.println("Valor máximo: "+max+" en la posición "+posicion_max);
+        System.out.println("Valor mínimo: "+min+" en la posición "+posicion_min);
     }
 }
-// valor ya almacenado, falta almacenar posicion, calcular valor maximo, calcular valor minimo, ver sus correspondientes posiciones
