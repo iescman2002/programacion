d5d8b241046b59e4ab20931a89ef845b04f88a4d
Iker <iescman2002@gmail.com>
2025-10-20 12:48:00 +0200

    Ejercicio11 hecho con Arrays Combinados.

--- Practica2/src/Ejercicio11.java
+++ Practica2/src/Ejercicio11.java
@@ -1,10 +1,13 @@
 public class Ejercicio11 {
     public static void main(String[] ars) {
         int[] arrayInt = {1,2,3,4,5,6};
-        String resultado = "";
+        int[] ArrayInvertido = new int[arrayInt.length];    // NUevo array con las mismas posiciones que arrayInt
         for (int i = 0; i < arrayInt.length; i++) {
-            resultado = arrayInt[i] +" "+ resultado;
+            ArrayInvertido[i] = arrayInt[arrayInt.length-1-i];     // ArrayInvertido[i] es igual que el valor del tamaño del  arrayInt - 1 (-1 porque así obtenemos la ultima posicion) y -i para ir obteniendo el valor inverso.
         }
-        System.out.println("El orden de los valores del array invertidos serán: "+resultado);
+        System.out.print("El orden de los valores del array pero invertido será: ");
+        for (int i = 0; i < ArrayInvertido.length; i++) {
+            System.out.print(ArrayInvertido[i]+" ");
+        }
     }
 }
