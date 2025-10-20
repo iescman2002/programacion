8c14d3efa74ce974ba157463e369d7ac574dae22
Iker <iescman2002@gmail.com>
2025-10-20 13:38:18 +0200

    Ejercicio12 Terminado.

---
+++ Practica2/src/Ejercicio12.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio12 {
+    public static void main() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Introduzca su número de DNI: ");
+        int DNI = s.nextInt();
+        String[] abecedario= {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+        int resto = DNI % 23;
+        System.out.println("La letra del DNI es: "+abecedario[resto]);
+    }
+}
