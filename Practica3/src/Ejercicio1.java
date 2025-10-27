2ef50474ea5e5869d0a2592cafc6e32a448b17ec
Iker <iescman2002@gmail.com>
2025-10-27 13:36:45 +0100

    Ejercicio1 empezado

--- Practica3/src/Ejercicio1.java
+++ Practica3/src/Ejercicio1.java
@@ -1,5 +1,15 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-
+        double [][] doub = new double[3][7];
+        doub [0][0] = 0;
+        doub [0][1] = 30;
+        doub [0][2] = 2;
+        doub [0][5] = 5;
+        doub [1][0] = 75;
+        doub [1][4] = 0;
+        doub [2][2] = -2;
+        doub [2][3] = 9;
+        doub [2][5] = 11;
+        System.out.println("Fila 0"+doub[0][0]+doub[0][1]+doub[0][2]);
     }
 }
