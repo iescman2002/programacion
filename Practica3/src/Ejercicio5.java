fffc00c7a18a667c158027549020d5555f173b74
Iker <iescman2002@gmail.com>
2025-11-03 20:17:32 +0100

    Ejercicio5 en proceso.

--- Practica3/src/Ejercicio5.java
+++ Practica3/src/Ejercicio5.java
@@ -22,13 +22,40 @@ public class Ejercicio5 {
                 System.out.println("El valor introducido es: "+array_bi[i][j]);
             }
         }
-        // NUMERO MAXIMO Y MINIMO: CALCULARLOS Y OBTENER POSICION
-        for (int i = 0; i < array_bi.length; i++) {
+        // NUMERO MAXIMO Y MINIMO: CALCULARLOS Y OBTENER POSICION --->>>> INTUYO DE USAR ARRAY AUXILIAR BIDIRECCIONAL PARA ALMACENAR POSICIONES DEL VALOR MAXIMO Y ARRAY NORMAL PARA EL VALOR
+        /*for (int i = 0; i < array_bi.length; i++) {
             for (int j = 0; j < array_bi[i].length; j++) {
                 int max= array_bi[i][j];
                 int min= array_bi[i][j];
-
             }
+        }*/
+        // OBTENER SUMA DE TODAS LAS FILAS Y COLUMNAS:
+        int suma_total = 0;
+        for (int i = 0; i < array_bi.length; i++) {
+            for (int j = 0; j < array_bi[i].length; j++) {
+                suma_total += array_bi[i][j];
+            }
+        }
+        System.out.println("La suma total será: "+suma_total);
+        // OBTENER SUMA DE FILAS
+        int[] suma_filas = new int[array_bi.length];
+        for (int i = 0; i < array_bi.length; i++) {
+            for (int j = 0; j < array_bi[i].length; j++) {
+                suma_filas[i] += array_bi[i][j];
+            }
+        }
+        for (int i = 0; i < suma_filas.length; i++) {
+            System.out.println("La suma de la fila "+i+" es : "+suma_filas[i]);
+        }
+        // OBTENER SUMA DE COLUMNAS
+        int[] suma_columnas = new int[array_bi[0].length];
+        for (int i = 0; i < array_bi.length; i++) {
+            for (int j = 0; j < array_bi[i].length; j++) {
+                suma_columnas[j] += array_bi[i][j];
+            }
+        }
+        for (int i = 0; i < suma_columnas.length; i++) {
+            System.out.println("La suma de la columna "+i+" es: "+suma_columnas[i]);
         }
     }
 }
