f23dc304ca765a38cedc37750266b689e9a4d036
Iker <iescman2002@gmail.com>
2025-11-26 13:41:23 +0100

    Practica4 Clase Area terminado.

--- Practica4/src/Area.java
+++ Practica4/src/Area.java
@@ -6,18 +6,21 @@ public class Area {
     private Hospital hospitales; // -> Referencia a hospital
     private Medico medicos;
     private int numMedicos; // contador de medicos por area
+
     // Crear constructor
-    public Area(String nombre, int id, int planta,Hospital hospital) {
+    public Area(String nombre, int id, int planta, Hospital hospital) {
         this.nombre = nombre;
         this.identificador = id;
         this.planta = planta;
         this.hospitales = hospital;
         this.numMedicos = 0;
     }
+
     // Metodos Get y Set
     public String getNombre() {
         return this.nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -25,6 +28,7 @@ public class Area {
     public int getIdentificador() {
         return this.identificador;
     }
+
     public void setIdentificador(int id) {
         this.identificador = id;
     }
@@ -32,19 +36,23 @@ public class Area {
     public int getPlanta() {
         return this.planta;
     }
+
     public void setPlanta(int planta) {
         this.planta = planta;
     }
 
-    public Hospital hospitales(){
+    public Hospital hospitales() {
         return this.hospitales;
     }
+
     public void setHospitales(Hospital hospital) {
         this.hospitales = hospital;
     }
+
     public int getNumMedicos() {
         return this.getNumMedicos();
     }
+
     public void setNumMedicos(int i) {
         this.numMedicos = i;
     }
@@ -52,7 +60,22 @@ public class Area {
     public void agregarMedico(Medico medicos) { // METODO AGREGAR MEDICO AL AREA
         this.medicos = medicos;
     }
+
     public void IncrementarMedico() {   // Metodo que Incrementa numMedicos +1
         this.numMedicos++;
+    }
+
+    public String compararMedicos(Area otraArea) { // Metodo que indica que area tiene mas medicos
+        if (this.numMedicos > (otraArea.numMedicos)) {
+            return "La area " + this.nombre + "es mayor que " + otraArea.getNombre();
+        } else if (this.numMedicos < (otraArea.numMedicos)) {
+            return "La area " + otraArea.getNombre() + " es mayor que "+this.nombre;
+        }
+        else {
+            return "El area "+this.nombre+" y el area "+otraArea.getNombre()+" tienen los mismos médicos.";
+        }
+    }
+    public int calcularCapacidadRestante(int capacidadMaxima) { // Devuelve la cantidad de medicos que se pueden añadir antes de llegar al max
+        return capacidadMaxima-this.numMedicos;
     }
 }
