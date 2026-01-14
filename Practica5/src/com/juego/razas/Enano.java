c8b9f0d0cd54c364a6f5f19d5bae5381592f5767
Iker <iescman2002@gmail.com>
2026-01-14 19:35:57 +0100

    Balance de estadisticas a raza Enano

--- Practica5/src/com/juego/razas/Enano.java
+++ Practica5/src/com/juego/razas/Enano.java
@@ -3,6 +3,6 @@ package com.juego.razas;
 public class Enano extends ModificadoresRazas {
     // Crear constructor con los atributos ya hechos
     public Enano() {
-        super(110, 7, 5, 4, 4);
+        super(110, 7, 3, 4, 4);
     }
 }
