271aadf44526e9b14015a41e47c2014616fc7361
Iker <iescman2002@gmail.com>
2026-01-14 18:53:07 +0100

    Crear bonificadores en comun de clases

---
+++ Practica5/src/com/juego/clases/StatsClases.java
@@ -0,0 +1,55 @@
+package com.juego.clases;
+
+public class StatsClases {
+    // Atributos y sus modificadores que tendrán en común cada clase.
+    private int vida;
+    private int fuerza;
+    private int defensa;
+    private int inteligencia;
+    private int destreza;
+
+    // Crear constructor de las stats de las clases
+    public StatsClases(int vida, int fuerza, int defensa, int inteligencia, int destreza) {
+        this.vida = vida;
+        this.fuerza = fuerza;
+        this.defensa = defensa;
+        this.inteligencia = inteligencia;
+        this.destreza = destreza;
+    }
+
+    // Crear metodos Get y Set
+    public int getVida() {
+        return this.vida;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getFuerza() {
+        return this.fuerza;
+    }
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public int getDefensa() {
+        return this.defensa;
+    }
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getInteligencia() {
+        return this.inteligencia;
+    }
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getDestreza() {
+        return this.destreza;
+    }
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+}
