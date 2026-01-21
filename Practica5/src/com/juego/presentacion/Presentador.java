b676408d53cf7c51bf00c3329f1cbe50ccdc54b0
Iker <iescman2002@gmail.com>
2026-01-21 12:54:30 +0100

    Añadir constructor al presentador.

--- Practica5/src/com/juego/presentacion/Presentador.java
+++ Practica5/src/com/juego/presentacion/Presentador.java
@@ -1,5 +1,6 @@
 package com.juego.presentacion;
 
+import com.juego.modelo.Combate;
 import com.juego.modelo.GestorPersonajes;
 
 import java.util.Scanner;
@@ -7,18 +8,19 @@ import java.util.Scanner;
 public class Presentador {
     Scanner s = new Scanner (System.in);
     public Presentador(){
+        inicio();
     }
     // Crear metodo menu principal
     public void inicio() {
         System.out.print(
-                "1. JUGAR"+"\n" +
+                "1. Jugar"+"\n" +
                 "2. Crear personaje"+"\n" +
                 "3. Salir"+
                 "\nIntroduzca a continuación su opción: ");
         int opcion = s.nextInt();
         switch (opcion) {
             case 1:
-
+                new Combate();
                 break;
             case 2:
                 new GestorPersonajes();
