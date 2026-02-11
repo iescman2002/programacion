9e1ae6d58bfa613dbe38357588dc07d030e179d3
Iker <iescman2002@gmail.com>
2026-02-11 23:23:40 +0100

    Ejercicio 13 Bloque 3 terminado.

--- Practica6/src/Bloques/Bloque3.java
+++ Practica6/src/Bloques/Bloque3.java
@@ -1,9 +1,6 @@
 package Bloques;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Bloque3 {
     // Ejericicio 11: Sistema de Comercio y Precios Dinámicos
@@ -21,9 +18,9 @@ public class Bloque3 {
         // Crear hashmap con ciudades (Claves) y los items en stock (valores)
         HashMap<String, ArrayList<String>> ciudades_stock = new HashMap<>();
         // Rellenar ciudades
-        ArrayList<String> ciudad1= new ArrayList<>();
-        ArrayList<String> ciudad2= new ArrayList<>();
-        ArrayList<String> ciudad3= new ArrayList<>();
+        ArrayList<String> ciudad1 = new ArrayList<>();
+        ArrayList<String> ciudad2 = new ArrayList<>();
+        ArrayList<String> ciudad3 = new ArrayList<>();
 
         ciudad1.add("Manzana");
         ciudad1.add("Pan");
@@ -41,50 +38,52 @@ public class Bloque3 {
         ciudad3.add("Cafe");
 
         ciudades_stock.put("Jerez", ciudad1);
-        ciudades_stock.put("Granada",ciudad2);
-        ciudades_stock.put("Murcia",ciudad3);
+        ciudades_stock.put("Granada", ciudad2);
+        ciudades_stock.put("Murcia", ciudad3);
 
         // Imprimir ciudades y el precio de los items
         System.out.println("Bloque 3 Ejercicio 11:");
-        System.out.println("Ciudades: \n"+ciudades_stock);
-        System.out.println("Precios sin aumentar impuestos:\n"+items);
+        System.out.println("Ciudades: \n" + ciudades_stock);
+        System.out.println("Precios sin aumentar impuestos:\n" + items);
         // comprobar si tiene la ciudad un stock > 5
         for (ArrayList<String> ciudad : ciudades_stock.values()) {
             if (tieneQueSubirImpuestos(ciudad)) { // Si la ciudad tiene que subir los impuestos:
-                items = aplicarImpuestoDeLujo(items,ciudad); // Sobrescribimos en la lista de items-precio con los precios actuales
+                items = aplicarImpuestoDeLujo(items, ciudad); // Sobrescribimos en la lista de items-precio con los precios actuales
             }
         }
-        System.out.println("Precio tras los impuestos: "+items);
+        System.out.println("Precio tras los impuestos: " + items);
     }
-        // Ejercicio 11 pt2 (Reto): Aplicar impuesto de lujo
-        private HashMap<String, Double> aplicarImpuestoDeLujo(HashMap<String,Double> items, ArrayList<String> ciudad_que_aplica) { // Se le pasa la información de los items y la ciudad a la que tendremos que aplicar el descuento
-            // Creamos hashmap vacio para devolver despues el hashmap de los items tras el impuesto
-            HashMap<String, Double> objetos_actualizados = new HashMap<>(items);
-            // Recorremos todas las claves de los items (nombres)
-                for (String claveItem : items.keySet()) {
-                    // y recorremos para cada clave un item diferente del array para comprobar si ese item es uno de los que hay que subirles el impuesto o no
-                    for (String itemDeLaCiudad : ciudad_que_aplica) {
-                        // Si está la clave del item en la lista de los items de la ciudad:
-                        if (claveItem.equals(itemDeLaCiudad)) {
-                            // Cambiamos el valor correspondiente a la clave (precio) a un 10% más del valor actual.
-                            double precio_actualizado = items.get(claveItem)*1.10;
-                            objetos_actualizados.put(claveItem,precio_actualizado);
-                        }
-                    }
+
+    // Ejercicio 11 pt2 (Reto): Aplicar impuesto de lujo
+    private HashMap<String, Double> aplicarImpuestoDeLujo(HashMap<String, Double> items, ArrayList<String> ciudad_que_aplica) { // Se le pasa la información de los items y la ciudad a la que tendremos que aplicar el descuento
+        // Creamos hashmap vacio para devolver despues el hashmap de los items tras el impuesto
+        HashMap<String, Double> objetos_actualizados = new HashMap<>(items);
+        // Recorremos todas las claves de los items (nombres)
+        for (String claveItem : items.keySet()) {
+            // y recorremos para cada clave un item diferente del array para comprobar si ese item es uno de los que hay que subirles el impuesto o no
+            for (String itemDeLaCiudad : ciudad_que_aplica) {
+                // Si está la clave del item en la lista de los items de la ciudad:
+                if (claveItem.equals(itemDeLaCiudad)) {
+                    // Cambiamos el valor correspondiente a la clave (precio) a un 10% más del valor actual.
+                    double precio_actualizado = items.get(claveItem) * 1.10;
+                    objetos_actualizados.put(claveItem, precio_actualizado);
                 }
-                // Devolvemos el HashMap objetos_actualizados; Por defecto devuelve la misma lista pero si se modifica algun precio se devolverá la lista con el precio actualizado
-                return objetos_actualizados;
-        }
-        // Ejercicio 11 pt2: Funcion de ayuda para comprobar si se sube o no los impuestos en la ciudad
-        private boolean tieneQueSubirImpuestos(ArrayList<String> ciudad) {
-            // Si la ciudad tiene mas de 5 items devolvemos que si tiene q subir los impuestos.
-            if (ciudad.size()>5) {
-                return true;
-            }
-            else {
-                return false;
             }
         }
+        // Devolvemos el HashMap objetos_actualizados; Por defecto devuelve la misma lista pero si se modifica algun precio se devolverá la lista con el precio actualizado
+        return objetos_actualizados;
+    }
+
+    // Ejercicio 11 pt2: Funcion de ayuda para comprobar si se sube o no los impuestos en la ciudad
+    private boolean tieneQueSubirImpuestos(ArrayList<String> ciudad) {
+        // Si la ciudad tiene mas de 5 items devolvemos que si tiene q subir los impuestos.
+        if (ciudad.size() > 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // Ejercicio 12: Gestión de Árboles de Habilidades
     public void gestionArbolesdeHabilidades() {
         System.out.println("Bloque 3: Ejercicio 12: ");
@@ -94,34 +93,37 @@ public class Bloque3 {
         HashMap<String, Boolean> habilidadesPaladin = new HashMap<>();
         HashMap<String, Boolean> habilidadesDruida = new HashMap<>();
         // Rellenar HashMap de habilidades
-        habilidadesSacerdote.put("Golpe Divino",true);
-        habilidadesSacerdote.put("Enfoque",false);
+        habilidadesSacerdote.put("Golpe Divino", true);
+        habilidadesSacerdote.put("Enfoque", false);
         habilidadesSacerdote.put("Escudo de Luz", false);
         habilidadesPaladin.put("Sigilo", false);
         habilidadesPaladin.put("Grito de Guerra", false);
         habilidadesPaladin.put("Espadazo", true);
-        habilidadesDruida.put("Curación",true);
+        habilidadesDruida.put("Curación", true);
         habilidadesDruida.put("Llamarada", true);
         habilidadesDruida.put("Veneno", false);
 
         // Asignar habilidades a la clase
-        clases.put("Sacerdote",habilidadesSacerdote);
-        clases.put("Paladin",habilidadesPaladin);
+        clases.put("Sacerdote", habilidadesSacerdote);
+        clases.put("Paladin", habilidadesPaladin);
         clases.put("Druida", habilidadesDruida);
         // LLamar a la funcion que verifica si tiene desbloqueada la Habilidad Enfoque al desbloquear Golpe Divino
         verificarHabilidadDesbloqueada(habilidadesSacerdote);
     }
-        // Ejercicio 12 Reto:
-        private void verificarHabilidadDesbloqueada(HashMap<String, Boolean> habilidades) {
-            // Cuando se desbloquee la habilidad
-            if (habilidades.get("Golpe Divino").equals(true)) { // Si golpe divino pasa a ser true (desbloqueada):
-                // verificamos si el jugador tiene desbloqueado enfoque
-                if (habilidades.get("Enfoque").equals(true)) { // Si está desbloqueada ("Enfoque",true):
-                    System.out.println("El jugador tiene la habilidad Enfoque desbloqueada.");
-                }
-                else {System.out.println("Error: Enfoque no ha sido aprendido.");} // Si tiene desbloqueado Golpe Divino pero Enfoque no, imprime
-            }
+
+    // Ejercicio 12 Reto:
+    private void verificarHabilidadDesbloqueada(HashMap<String, Boolean> habilidades) {
+        // Cuando se desbloquee la habilidad
+        if (habilidades.get("Golpe Divino").equals(true)) { // Si golpe divino pasa a ser true (desbloqueada):
+            // verificamos si el jugador tiene desbloqueado enfoque
+            if (habilidades.get("Enfoque").equals(true)) { // Si está desbloqueada ("Enfoque",true):
+                System.out.println("El jugador tiene la habilidad Enfoque desbloqueada.");
+            } else {
+                System.out.println("Error: Enfoque no ha sido aprendido.");
+            } // Si tiene desbloqueado Golpe Divino pero Enfoque no, imprime
         }
+    }
+
     // Ejercicio 13: Historial de Incursiones
     public void historialDeIncursiones() {
         System.out.println("Bloque 3 Ejercicio 13:");
@@ -138,14 +140,14 @@ public class Bloque3 {
         participantes_raid1.add("Manuel");
 
         HashSet<String> participantes_raid2 = new HashSet<>();
-        participantes_raid1.add("Manuel");
-        participantes_raid1.add("Raul");
-        participantes_raid1.add("Jose");
+        participantes_raid2.add("Manuel");
+        participantes_raid2.add("Raul");
+        participantes_raid2.add("Jose");
 
         HashSet<String> participantes_raid3 = new HashSet<>();
-        participantes_raid1.add("Manuel");
-        participantes_raid1.add("Alicia");
-        participantes_raid1.add("Elena");
+        participantes_raid3.add("Manuel");
+        participantes_raid3.add("Alicia");
+        participantes_raid3.add("Elena");
 
         // Añadir participantes a la raid
         raid1.add(participantes_raid1);
@@ -155,5 +157,30 @@ public class Bloque3 {
         mazmorrasCompletadas.put("Mazmorra del duende", raid1);
         mazmorrasCompletadas.put("Mazmorra infernal", raid2);
         mazmorrasCompletadas.put("Mazmorra de los lamentos", raid3);
+        // Ejercicio 13 Reto:
+        // Recorrer todas las listas (valores) de mazmorrasCompletadas (obtener personaje que mas veces sale)
+        HashMap<String, Integer> personaje_veces_q_sale = new HashMap<>(); // Hashmap auxiliar que guarda un personaje (clave) y las veces que ha ido a una raid (valor)
+        for (List<HashSet<String>> lista : mazmorrasCompletadas.values()) { // Recorrer las listas (valor) de mazmorras completadas
+            for (HashSet<String> raid : lista) { // Recorro las raids que se hacen en la lista
+                for (String personaje : raid) { // Recorro los personajes de dentro de las raids
+                    if (personaje_veces_q_sale.containsKey(personaje)) { // Si personaje ya esta introducido en el hashmap (como clave):
+                        personaje_veces_q_sale.put(personaje, personaje_veces_q_sale.get(personaje) + 1); // Sobrescribimos la clave con el personaje y al valor le aumentamos +1 a las veces q ha ido ya
+                    } else { // Si no estaba introducida en la lista (lo añadimos al hashmap):
+                        personaje_veces_q_sale.put(personaje, 1); // Introducimos de clave el personaje y le damos el valor 1 al conjunto de la raid.
+                    }
+                }
+            }
+        }
+        //Imprimir el personaje mas repetido
+        String nombre_mejor = "";
+        Integer veces = 0;
+        for (String personaje : personaje_veces_q_sale.keySet()) { // Recorro los nombres (clave) del Hashmap de las veces que sale un personaje
+            if (personaje_veces_q_sale.get(personaje)>veces) {
+                nombre_mejor = personaje;
+                veces = personaje_veces_q_sale.get(personaje);
+            }
+
+        }
+        System.out.println("El personaje que más raids ha hecho es: "+nombre_mejor+" con un total de : "+veces);
     }
 }
