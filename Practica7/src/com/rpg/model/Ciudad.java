4f38d1db01574e1890205e9d0e879337bdcad2cc
Iker <iescman2002@gmail.com>
2026-02-20 10:53:19 +0100

    Crear clase de ciudad.

--- Practica7/src/com/rpg/model/Ciudad.java
+++ Practica7/src/com/rpg/model/Ciudad.java
@@ -1,4 +1,44 @@
 package com.rpg.model;
 
 public class Ciudad {
+    // Crear atributos
+    private String nombre;
+    private String poblacion;
+    private String clima;
+    private String riesgo;
+    // Crear constructor
+    public Ciudad(String nombre, String poblacion, String clima, String riesgo) {
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+        this.clima = clima;
+        this.riesgo = riesgo;
+    }
+    // Metodos Getter y Setter
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPoblacion() {
+        return this.poblacion;
+    }
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getClima() {
+        return this.clima;
+    }
+    public void setClima(String clima) {
+        this.clima = clima;
+    }
+
+    public String getRiesgo() {
+        return this.riesgo;
+    }
+    public void setRiesgo(String riesgo) {
+        this.riesgo = riesgo;
+    }
 }
