570289183226ba8ee2592bce9f16a4702e779d8a
Iker <iescman2002@gmail.com>
2026-02-23 14:21:59 +0100

    empezar txt helper

--- Practica7/src/com/rpg/utils/TxtHelper.java
+++ Practica7/src/com/rpg/utils/TxtHelper.java
@@ -1,4 +1,28 @@
 package com.rpg.utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class TxtHelper {
+    // Leer ficheros
+    private File file;
+    private FileReader fr;
+    private BufferedReader br;
+
+    // Constructor
+    public TxtHelper() {
+        this.file = Files.newBufferedReader( Paths.get("../../Ficheros/ciudades.txt"));
+        this.fr = new FileReader(file);
+        this.br = new BufferedReader(fr);
+    }
+
+    public void leerLineas() {
+        String linea;
+        while ((linea = file.readLine()) != null) {
+            System.out.println(linea);
+        }
+    }
 }
