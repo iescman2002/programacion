0f8515be9600473089b4b8a958fcf8074c2f5778
Iker <iescman2002@gmail.com>
2026-02-25 10:39:57 +0100

    Crear funcion leer fichero.

--- Practica7/src/com/rpg/utils/TxtHelper.java
+++ Practica7/src/com/rpg/utils/TxtHelper.java
@@ -3,26 +3,27 @@ package com.rpg.utils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
 
-public class TxtHelper {
+public class TxtHelper{
     // Leer ficheros
     private File file;
     private FileReader fr;
     private BufferedReader br;
 
     // Constructor
-    public TxtHelper() {
-        this.file = Files.newBufferedReader( Paths.get("../../Ficheros/ciudades.txt"));
-        this.fr = new FileReader(file);
-        this.br = new BufferedReader(fr);
+    public TxtHelper() throws IOException {
+        this.file = new File("../../Ficheros/ciudades.txt"); // Crear fichero ciudades.txt
+        this.fr = new FileReader(file); // Para poder leer un fichero
+        this.br = new BufferedReader(fr); // Para leer mas de un caracter
     }
 
-    public void leerLineas() {
-        String linea;
-        while ((linea = file.readLine()) != null) {
+    public void leerLineas() throws IOException {
+        String linea = this.br.readLine();
+        while (linea != null) {
             System.out.println(linea);
+            linea = br.readLine();
         }
+        fr.close();
     }
 }
