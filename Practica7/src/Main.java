92d6cb1ff98f2f179bae3bd5802c53df763635ab
Iker <iescman2002@gmail.com>
2026-03-10 19:02:31 +0100

    Pasar el codigo de Main a GestionMundo (metodo cargarTodo).

--- Practica7/src/Main.java
+++ Practica7/src/Main.java
@@ -1,46 +1,10 @@
-import com.rpg.model.Ciudades;
-import com.rpg.model.Items;
-import com.rpg.model.Personajes;
-import com.rpg.utils.JsonHelper;
-import com.rpg.utils.TxtHelper;
+import com.rpg.services.GestionMundo;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /* Leer fichero txt de forma temporal*/
-        TxtHelper txthelper = new TxtHelper();
-        txthelper.leerLineas();
-
-        /* Obtener los datos del txt !!!! ESTO HAY QUE METERLO PARA SUSTITUIR AL METODO ACTUAL EN EL TXTHELPER*/
-        List<Ciudades> ciudades = new ArrayList<>(); /* Se crea una lista que contendra las ciudades*/
-        List<String> lineas = Files.readAllLines(Paths.get("Practica7/Ficheros/ciudades.txt")); /* Se crea una lista donde se guarda el contenido del fichero*/
-        for (String linea : lineas) {
-            String[] s = linea.split(";"); /* Indicamos el separador de los campos que es ; y guardamos los correspondientes atributos en el array */
-            Ciudades c = new Ciudades( /* Creamos una nueva ciudad para cada linea del fichero*/
-                        s[0],                       /* Atributo nombre */
-                        Integer.parseInt(s[1]),     /* Atributo poblacion (pasamos el dato String que es como lo teniamos guardado a Integer) */
-                        s[2],                       /* Atributo clima */
-                        Integer.parseInt(s[3])      /* Atributo riesgo (pasamos el dato String que es como lo teniamos guardado a Integer)*/
-                    );
-            ciudades.add(c); /* Añadimos la ciudad a la lista de ciudades*/
-        }
-
-        /* Leer archivos json de forma temporal*/
-        JsonHelper jsonHelper = new JsonHelper();
-        List<Personajes> personajes= jsonHelper.readList("Practica7/Ficheros/personajes.json", Personajes.class);
-        for (Personajes personaje : personajes) {
-            System.out.println(personaje);
-        }
-
-        List<Items> items = jsonHelper.readList("Practica7/Ficheros/Items.json", Items.class);
-        for (Items item : items) {
-            System.out.println(item);
-        }
+        new GestionMundo();
 
         /*Escribir datos en el json
         jsonHelper.writeList("Practica7/Ficheros/personajes.json",personajes);
--- Practica7/src/com/rpg/services/GestionMundo.java
+++ Practica7/src/com/rpg/services/GestionMundo.java
@@ -1,4 +1,43 @@
 package com.rpg.services;
 
+import com.rpg.model.*;
+import com.rpg.utils.JsonHelper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestionMundo {
+    // Inicializar listas de objetos
+    List<Personajes> personajes;
+    List<Items> items;
+    List<Ciudades> ciudades;
+
+    public GestionMundo() throws IOException {
+        personajes = new ArrayList<>();
+        items = new ArrayList<>();
+        ciudades = new ArrayList<>();
+
+        cargarTodo();
+    }
+
+    public void cargarTodo() throws IOException {
+        // Rellenar las listas de objetos
+        this.personajes= new JsonHelper().readList("Practica7/Ficheros/personajes.json", Personajes.class);
+        this.items = new JsonHelper().readList("Practica7/Ficheros/Items.json", Items.class);
+
+        List<String> lineas = Files.readAllLines(Paths.get("Practica7/Ficheros/ciudades.txt")); /* Se crea una lista donde se guarda el contenido del fichero*/
+        for (String linea : lineas) {
+            String[] s = linea.split(";"); /* Indicamos el separador de los campos que es ; y guardamos los correspondientes atributos en el array */
+            Ciudades c = new Ciudades( /* Creamos una nueva ciudad para cada linea del fichero */
+                    s[0],                       /* Atributo nombre */
+                    Integer.parseInt(s[1]),     /* Atributo poblacion (pasamos el dato String que es como lo teniamos guardado a Integer)*/
+                    s[2],                       /* Atributo clima */
+                    Integer.parseInt(s[3])      /* Atributo riesgo (pasamos el dato String que es como lo teniamos guardado a Integer)*/
+            );
+            this.ciudades.add(c); /* Añadimos la ciudad a la lista de ciudades*/
+        }
+    }
 }
