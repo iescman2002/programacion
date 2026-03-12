b89ced046ba185d5917315daa53da75280a58fda
Iker <iescman2002@gmail.com>
2026-03-12 19:07:47 +0100

    usar metodo crearPersonaje.

--- Practica7/src/Main.java
+++ Practica7/src/Main.java
@@ -1,13 +1,10 @@
 import com.rpg.handler.*;
 import com.rpg.services.GestionMundo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws RecursoNoEncontradoException, DatoInvalidoException, FormatoInvalidoException {
         GestionMundo gm = new GestionMundo();
+        gm.crearPersonaje();
         gm.guardarCambios();
         gm.informeFinal();
     }
