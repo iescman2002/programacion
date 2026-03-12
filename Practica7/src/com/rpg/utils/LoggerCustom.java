ffd088bae461b462783244bbb3ecd6d462674140
Iker <iescman2002@gmail.com>
2026-03-12 19:45:50 +0100

    Crear fichero errores.log y Hacer el LoggerCustom.

--- Practica7/src/com/rpg/utils/LoggerCustom.java
+++ Practica7/src/com/rpg/utils/LoggerCustom.java
@@ -1,4 +1,28 @@
 package com.rpg.utils;
 
+import com.rpg.handler.DatoInvalidoException;
+import com.rpg.handler.RecursoNoEncontradoException;
+
+import java.io.*;
+
 public class LoggerCustom {
+    String path;
+
+    public LoggerCustom() {
+        this.path = "Practica7/Ficheros/errores.log";
+    }
+
+    public void escribirLog(String mensaje) throws RecursoNoEncontradoException, DatoInvalidoException {
+        try { // Leer el fichero
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path, true));
+            writer.write(mensaje);
+            writer.newLine();
+        }
+        catch (FileNotFoundException e) {
+            throw new RecursoNoEncontradoException("El fichero: " + e.getMessage() + " no ha sido encontrado.");
+        }
+        catch (IOException e) {
+            throw new DatoInvalidoException("No se ha podido escribir en el fichero logs.");
+        }
+    }
 }
