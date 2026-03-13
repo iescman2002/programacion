29b3aa11742dd460db74e5d93696a97f40639d77
Iker <iescman2002@gmail.com>
2026-03-13 01:53:41 +0100

    Modificar el tipo de la lista de equipoIds de String a Items, añadir validacion cruzada en los items de los personajes, añadir hashmap que requiere la practica. Eliminar personajes creados anteriormente con equipoId incorrecta. PRACTICA TERMINADA.

--- Practica7/src/com/rpg/services/GestionMundo.java
+++ Practica7/src/com/rpg/services/GestionMundo.java
@@ -8,52 +8,90 @@ import com.rpg.utils.LoggerCustom;
 import com.rpg.utils.TxtHelper;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class GestionMundo {
     // Inicializar listas de objetos
     List<Personajes> personajes;
     List<Items> items;
     List<Ciudades> ciudades;
+    // Crear Hashmap para buscar items por id de forma eficiente
+    HashMap<String, Items> coleccionDeItems; // La clave String será el idItem y el valor será el propio item
 
     public GestionMundo() throws RecursoNoEncontradoException, DatoInvalidoException {
-        personajes = new ArrayList<>();
-        items = new ArrayList<>();
-        ciudades = new ArrayList<>();
-
+        this.personajes = new ArrayList<>();
+        this.items = new ArrayList<>();
+        this.ciudades = new ArrayList<>();
+        this.coleccionDeItems = new HashMap<>();
         cargarTodo();
     }
 
     public void cargarTodo() throws RecursoNoEncontradoException, DatoInvalidoException {
         // Rellenar las listas de objetos
-        /*try { */ // Eliminamos el try catch porque la función readList y leerLineas ya tiene un try catch incorporado entonces meter otro seria duplicar el error
-            this.personajes= new JsonHelper().readList("Practica7/Ficheros/personajes.json", Personajes.class);
-            new LoggerCustom().escribirLog("["+ LocalDateTime.now()+"] INFO: Lista de personajes cargada con exito.");
-            this.items = new JsonHelper().readList("Practica7/Ficheros/Items.json", Items.class);
-            new LoggerCustom().escribirLog("["+ LocalDateTime.now()+"] INFO: Lista de Items cargada con exito.");
-            this.ciudades = new TxtHelper().leerLineas("Practica7/Ficheros/ciudades.txt");
-            new LoggerCustom().escribirLog("["+ LocalDateTime.now()+"] INFO: Lista de Ciudades cargada con exito.");
-        /*}
-        catch (Exception e) { // Lanza el mensaje de error
-            new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] ERROR: El fichero "+e.getMessage()+" no ha sido encontrado.");
-            throw new RecursoNoEncontradoException("Error: El fichero "+e.getMessage()+" no existe o no ha sido encontrado.");
-        }*/
+        this.personajes= new JsonHelper().readList("Practica7/Ficheros/personajes.json", Personajes.class);
+        new LoggerCustom().escribirLog("["+ LocalDateTime.now()+"] INFO: Lista de personajes cargada con exito.");
+        this.items = new JsonHelper().readList("Practica7/Ficheros/Items.json", Items.class);
+        new LoggerCustom().escribirLog("["+ LocalDateTime.now()+"] INFO: Lista de Items cargada con exito.");
+        this.ciudades = new TxtHelper().leerLineas("Practica7/Ficheros/ciudades.txt");
+        new LoggerCustom().escribirLog("["+ LocalDateTime.now()+"] INFO: Lista de Ciudades cargada con exito.");
+        // Rellenar el HashMap con los items
+        for (Items item : this.items) {
+            coleccionDeItems.put(item.getId(),item);
+        }
+
+        // Validar los idItems de los personajes (Asegurarse que los idItems existan)
+        validacion(); // Metido en una función aparte para poder validar despues de crear un personaje tambien sin copiar codigo.
     }
+
     public void crearPersonaje() throws DatoInvalidoException, RecursoNoEncontradoException {
         // Probar que se pueden agregar personajes
         try {
             Scanner s = new Scanner(System.in);
             System.out.print("Introduzca el nombre del personaje: ");
             String nombre = s.next();
+            new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] INFO: El usuario ha introducido correctamente el nombre.");
             System.out.print("Introduzca la raza del personaje: ");
             String raza = s.next();
+            new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] INFO: El usuario ha introducido correctamente la raza.");
+            // Escoger nivel
             System.out.print("Introduzca el nivel del personaje: ");
-            Integer nivel = s.nextInt();
+            Integer nivel;
+            while (true) {
+                nivel = s.nextInt();
+                if (nivel >= 0) {
+                    new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] INFO: El usuario ha introducido correctamente el nivel.");
+                    break;
+                }
+                else {
+                    System.out.print("El nivel tiene que ser superior o igual a 0, intentelo de nuevo: ");
+                    new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] INFO: El usuario ha introducido un nivel negativo, volviendolo a intentar.");
+                }
+            }
+            // Elegir los items para el usuario
+            List<String> listaDeItems = new ArrayList<>();
+            // Los muestra
+            System.out.println("De estos siguientes items: ");
+            for (Items item : coleccionDeItems.values()) {
+                System.out.println(item.toString());
+            }
+            // Bucle para escoger un item correcto
+            while(true) {
+                System.out.print("Introduzca el id del item que quiere agregar o introduzca Salir para salir: ");
+                String idEscogido = s.next();
+                if (idEscogido.equalsIgnoreCase("Salir")) { // Si lo que hemos introducido es igual a salir (da igual las mayusculas o minusculas lo que mira son los caracteres) se sale
+                    break;
+                }
+                if (!coleccionDeItems.containsKey(idEscogido)) { // Si el usuario ha introducido un id que NO existe en la coleccion de items
+                    new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] ERROR: El idItem "+idEscogido+" introducido no existe en el sistema.");
+                }
+                else {
+                    listaDeItems.add(idEscogido); // Añadimos a la lista el item escogido
+                }
+            }
             // Agregar nuevo personaje (igual que si crearamos un objeto)
-            Personajes personaje = new Personajes(nombre, raza, nivel, new ArrayList<>());
+            Personajes personaje = new Personajes(nombre, raza, nivel, listaDeItems);
             this.personajes.add(personaje); // Lo añadimos a la lista
+            validacion(); // y lo validamos para asegurarnos que lo hayamos creado bien
             new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] INFO: El personaje "+personaje.getNombre()+" ha sido creado.");
         }
         catch (Exception e) {
@@ -97,5 +135,20 @@ public class GestionMundo {
         }
         new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] INFO: Se han impreso correctamente todos los personajes.");
         System.out.println("------------------");
+    }
+
+    // Metodo para validar que los items de la lista de cada personaje exista.
+    private void validacion() throws RecursoNoEncontradoException, DatoInvalidoException {
+        for (Personajes personaje : personajes) { // Recorro los personajes
+            for (String idPersonaje : personaje.getEquipoIds()) {
+                if (coleccionDeItems.containsKey(idPersonaje)) { // Si la clave es igual al idPersonaje (es decir que existe el item)
+                    new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] INFO: El item"+ coleccionDeItems.get(idPersonaje).getId() +" del personaje "+personaje.getNombre() +" se ha validado correctamente.");
+                }
+                else { // Si el item que busca no se encuentra
+                    new LoggerCustom().escribirLog("["+LocalDateTime.now()+"] ERROR: El item "+idPersonaje+ " del personaje "+personaje+" no existe.");
+                    throw new DatoInvalidoException("Error: El item asociado al personaje no existe.");
+                }
+            }
+        }
     }
 }
