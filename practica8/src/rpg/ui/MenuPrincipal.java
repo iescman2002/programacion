54a31991b87a5cfff6db38b1765166f356509cd5
iker <ikersan2007@gmail.com>
2026-04-10 09:19:45 +0200

    Empezar con el menu principal.

--- practica8/src/rpg/ui/MenuPrincipal.java
+++
@@ -1,4 +0,0 @@
-package rpg.ui;
-
-public class MenuPrincipal {
-}
---
+++ practica8/src/rpg/ui/Menus.java
@@ -0,0 +1,30 @@
+package rpg.ui;
+
+import java.util.Scanner;
+
+public class Menus {
+
+    Scanner s = new Scanner(System.in);
+    public Menus() {
+        cargarMenuPrincipal();
+    }
+
+    private void cargarMenuPrincipal(){
+        System.out.println("¡Bienvenido al XPG Guild Master!");
+        System.out.println("A continuación, indique que opción deseas acceder:");
+        System.out.println("1. Crear personaje.");
+        System.out.println("2. [SOON].");
+        System.out.println("3. [SOON].");
+        System.out.println("--------------------------------");
+
+        int opcion = s.nextInt();
+        switch (opcion) {
+            case 1:
+                new crearPersonaje();
+                break;
+            default:
+                System.out.print("Saliendo...");
+                break;
+        }
+    }
+}
