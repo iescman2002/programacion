8fe723f5cf8a8caafb9633ac135abdd517f6cad2
iker <iescman2002@gmail.com>
2026-04-15 20:44:20 +0200

    Modificar metodo que obtiene el daño del personaje para que tambien obtenga la defensa.

--- practica8/src/rpg/logic/MotorCombate.java
+++ practica8/src/rpg/logic/MotorCombate.java
@@ -32,12 +32,17 @@ public class MotorCombate {
         new Menus().menuElegirHabilidades(personaje1,1);
         new Menus().menuElegirHabilidades(personaje2,2);
         // Ahora, calculamos el daño total de cada personaje
-        Integer danio_pj1 = obtenerDanio(personaje1);
-        Integer danio_pj2 = obtenerDanio(personaje2);
+        Integer[] stats_pj1 = obtenerDanioYDefensa(personaje1);
+        Integer[] stats_pj2 = obtenerDanioYDefensa(personaje2);
 
+        Integer danio_pj1 = stats_pj1[0];
+        Integer danio_pj2 = stats_pj2[0];
+        // Ahora, calculamos la defensa total de cada personaje
+        Integer defensa_pj1 = stats_pj1[1];
+        Integer defensa_pj2 = stats_pj2[1];
     }
 
-    private Integer obtenerDanio(Personaje pj) throws SQLException {
+    private Integer[] obtenerDanioYDefensa(Personaje pj) throws SQLException {
         InventarioDAO inventarioDAO = new InventarioDAO();
         List<Inventario> inventarios = inventarioDAO.getInventarios();
 
@@ -50,22 +55,27 @@ public class MotorCombate {
             }
             // Si ya se ha añadido el item al hashmap sumo 1 al valor (cantidad)
             else if (inventario.getId_personaje().equals(pj.getId())&&itemsPJ.containsKey(inventario.getId_item())) {
-                itemsPJ.replace(inventario.getId_item(),itemsPJ.get(inventario.getId_item()+1));
+                itemsPJ.replace(inventario.getId_item(),itemsPJ.get(inventario.getId_item())+1);
             }
         }
 
         Integer dmgTotal_pj = 0;
+        Integer defTotal_pj = 0;
 
         ItemDAO itemDAO = new ItemDAO();
         List<Item> items = itemDAO.getItems();
         for (Item item : items) {
             if (itemsPJ.containsKey(item.getId())) {
                 // si el item se encuentra en los items del personaje,
-                Integer danio = item.getBonificador_ataque()*itemsPJ.get(item.getId()); // Daño de la habilidad = bono_ataque del item * cantidad de veces que tiene el item
+                Integer danio = item.getBonificador_ataque()*itemsPJ.get(item.getId()); // Daño del personaje = bono_ataque del item * cantidad de veces que tiene el item
+                Integer def = item.getBonificador_defensa()*itemsPJ.get(item.getId()); // Defensa del personaje = bono_ataque del item * cantidad de veces que tiene el item
                 dmgTotal_pj += danio;
+                defTotal_pj += def;
             }
         }
-
-        return dmgTotal_pj;
+        Integer[] stats = new Integer[2];
+        stats[0] =  dmgTotal_pj; // El daño del personaje en la posicion 0
+        stats[1] =  defTotal_pj; // La defensa del personaje en la posicion 1
+        return stats;
     }
 }
