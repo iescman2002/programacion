cf21b4b31d461600ff277a5b32007d3ac1e31b88
iker <iescman2002@gmail.com>
2026-04-15 20:29:25 +0200

    Crear metodo para obtener el daño que hara cada personaje en el combate.

--- practica8/src/rpg/logic/MotorCombate.java
+++ practica8/src/rpg/logic/MotorCombate.java
@@ -1,13 +1,19 @@
 package rpg.logic;
 
+import rpg.dao.InventarioDAO;
+import rpg.dao.ItemDAO;
 import rpg.dao.PersonajeDAO;
 import rpg.exception.DatoInvalidoException;
 import rpg.exception.LimiteHabilidadesException;
 import rpg.exception.RecursoNoEncontradoException;
+import rpg.model.Inventario;
+import rpg.model.Item;
 import rpg.model.Personaje;
 import rpg.ui.Menus;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MotorCombate {
@@ -25,5 +31,41 @@ public class MotorCombate {
         // Despues, elegir las habilidades de los personajes
         new Menus().menuElegirHabilidades(personaje1,1);
         new Menus().menuElegirHabilidades(personaje2,2);
+        // Ahora, calculamos el daño total de cada personaje
+        Integer danio_pj1 = obtenerDanio(personaje1);
+        Integer danio_pj2 = obtenerDanio(personaje2);
+
+    }
+
+    private Integer obtenerDanio(Personaje pj) throws SQLException {
+        InventarioDAO inventarioDAO = new InventarioDAO();
+        List<Inventario> inventarios = inventarioDAO.getInventarios();
+
+        HashMap<Integer,Integer> itemsPJ = new HashMap<>(); // La clave es el id_item y el valor la cantidad del item
+        // Obtener el id de los items del personaje
+        for (Inventario inventario : inventarios) {
+            // Si el item no esta añadido al hashmap lo añado y le pongo 1 de cantidad
+            if (inventario.getId_personaje().equals(pj.getId())&&(!itemsPJ.containsKey(inventario.getId_item()))) {
+                itemsPJ.put(inventario.getId_item(),1);
+            }
+            // Si ya se ha añadido el item al hashmap sumo 1 al valor (cantidad)
+            else if (inventario.getId_personaje().equals(pj.getId())&&itemsPJ.containsKey(inventario.getId_item())) {
+                itemsPJ.replace(inventario.getId_item(),itemsPJ.get(inventario.getId_item()+1));
+            }
+        }
+
+        Integer dmgTotal_pj = 0;
+
+        ItemDAO itemDAO = new ItemDAO();
+        List<Item> items = itemDAO.getItems();
+        for (Item item : items) {
+            if (itemsPJ.containsKey(item.getId())) {
+                // si el item se encuentra en los items del personaje,
+                Integer danio = item.getBonificador_ataque()*itemsPJ.get(item.getId()); // Daño de la habilidad = bono_ataque del item * cantidad de veces que tiene el item
+                dmgTotal_pj += danio;
+            }
+        }
+
+        return dmgTotal_pj;
     }
 }
